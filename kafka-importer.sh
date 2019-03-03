98a1a9ff5ab3063dcd2dcae73170131dd467eecc
Avi Gershon <avi.gershon@production-qa.com>
2019-03-03 18:05:05 +0200

    sdfs

--- kafka-importer.sh
+++ kafka-importer.sh
@@ -8,7 +8,7 @@ do
     if [ ! -f $file ]; then
       echo "File already been processed"
     else
-      topic=$(echo ${file/$DIRECTORY/$emptyString} | cut -d"/" -f2)
+      topic=$(echo ${file/$DIRECTORY/$emptyString} | cut -d"/" -f1)
       echo "Loading file $(basename "${file}") to topic ${topic}..."
       java -jar /usr/share/java/avro-tools-1.7.7.jar getschema "${file}" > "${file}".avsc
       java -jar /usr/share/java/avro-tools-1.7.7.jar tojson "${file}" > "${file}".json

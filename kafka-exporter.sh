cb90c5ff6c49196aa75eb72511d246d9f79aab5c
Avi Gershon <avi.gershon@production-qa.com>
2019-03-05 13:43:21 +0200

    check if system topic

--- kafka-exporter.sh
+++ kafka-exporter.sh
@@ -16,24 +16,30 @@ do
           s=${s#*"$delimiter"};
       done;
 
-      echo "Creating S3 sink connector to bucket ${array[0]}"
+      if ["${array[0]}" == "__confluent"]; then
+        echo "System topic - aborting..."
 
-      curl -X PUT \
-        -H 'Content-Type: application/json' \
-        -H 'Accept: application/json' \
-        -d '{
-        "connector.class": "io.confluent.connect.s3.S3SinkConnector",
-        "schema.compatibility": "FULL",
-        "flush.size": "6",
-        "topics": "'${topic}'",
-        "tasks.max": "1",
-        "s3.part.size": "5242880",
-        "store.url": "'$MINIO_URL'",
-        "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
-        "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
-        "schema.generator.class": "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator",
-        "storage.class": "io.confluent.connect.s3.storage.S3Storage",
-        "s3.bucket.name": "'${array[0]}'"
-      }' http://v1-cp-kafka-connect:8083/connectors/${topic}.s3.sink/config
+      else
 
+        echo "Creating S3 sink connector to bucket ${array[0]}"
+
+        curl -X PUT \
+          -H 'Content-Type: application/json' \
+          -H 'Accept: application/json' \
+          -d '{
+          "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+          "schema.compatibility": "FULL",
+          "flush.size": "6",
+          "topics": "'${topic}'",
+          "tasks.max": "1",
+          "s3.part.size": "5242880",
+          "store.url": "'$MINIO_URL'",
+          "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
+          "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+          "schema.generator.class": "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator",
+          "storage.class": "io.confluent.connect.s3.storage.S3Storage",
+          "s3.bucket.name": "'${array[0]}'"
+        }' http://v1-cp-kafka-connect:8083/connectors/${topic}.s3.sink/config
+
+      fi
 done
